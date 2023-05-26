94c2c749711e68c03cf7e1365bb07fa3a521e077
Hakim Kibas <abdulhakim1699@gmail.com>
2023-05-26 17:08:50 +0800

    friday

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import virus from './virus.png';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +33,13 @@ function App() {
         <h1>Bangsa</h1>
         <img src={virus} alt="virus" />
         <p>Hakim Was here</p>
-        <li> Budak up</li>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key = {activity.id}>
+                {activity.title}
+            </li>
+          ))}
+        </ul>
       </body>
     </div>
   );
